527607e48c4027e57741f0fca40e6c06c14faded
lukemcgrath98 <lukemcgrath98@gmail.com>
2021-03-30 12:41:22 +0100

    updated 3rd screen

--- fov_app/lib/third_page.dart
+++ fov_app/lib/third_page.dart
@@ -1,60 +1,287 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:swipedetector/swipedetector.dart';
+
+String pad(x) {
+  String padded;
+  if (x == "0" ||
+      x == "1" ||
+      x == "2" ||
+      x == "3" ||
+      x == "4" ||
+      x == "5" ||
+      x == "6" ||
+      x == "7" ||
+      x == "8" ||
+      x == "9") {
+    padded = x.padLeft(2, '0');
+    return padded;
+  } else {
+    return x;
+  }
+}
+
+void main() {
+  runApp(ThirdPage());
+}
 
 class ThirdPage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MusicApp(),
+    );
+  }
+}
+
+class MusicApp extends StatefulWidget {
+  @override
+  _MusicAppState createState() => _MusicAppState();
+}
+
+class _MusicAppState extends State<MusicApp> {
+  //we will need some variables
+  bool playing = false; // at the begining we are not playing any song
+  IconData playBtn = Icons.play_arrow; // the main state of the play button icon
+
+  //Now let's start by creating our music player
+  //first let's declare some object
+  AudioPlayer _player;
+  AudioCache cache;
+
+  Duration position = new Duration();
+  Duration musicLength = new Duration();
+
+  //we will create a custom slider
+
+  Widget slider() {
+    return Container(
+      width: 280.0,
+      child: Slider.adaptive(
+          activeColor: Colors.blue[800],
+          inactiveColor: Colors.grey[350],
+          value: position.inSeconds.toDouble(),
+          max: musicLength.inSeconds.toDouble(),
+          onChanged: (value) {
+            seekToSec(value.toInt());
+          }),
+    );
+  }
+
+  //let's create the seek function that will allow us to go to a certain position of the music
+  void seekToSec(int sec) {
+    Duration newPos = Duration(seconds: sec);
+    _player.seek(newPos);
+  }
+
+  //Now let's initialize our player
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _player = AudioPlayer();
+    cache = AudioCache(fixedPlayer: _player);
+
+    //now let's handle the audioplayer time
+
+    //this function will allow you to get the music duration
+    _player.durationHandler = (d) {
+      setState(() {
+        musicLength = d;
+      });
+    };
+
+    //this function will allow us to move the cursor of the slider while we are playing the song
+    _player.positionHandler = (p) {
+      setState(() {
+        position = p;
+      });
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
-        title: Text("Controls Page"),
-        backgroundColor: Colors.red,
+        title: Container(
+          child: Image.asset(
+            "images/logowhite.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        backgroundColor: Colors.grey,
       ),
-      body: SwipeDetector(
-        onSwipeRight: () {
-          Navigator.pop(context);
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // backgroundColor: Colors.red,
-          children: <Widget>[
-            Text("WELCOME TO CONTROL PAGE",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontStyle: FontStyle.italic,
-                  height: 3,
-                  fontSize: 30,
-                  wordSpacing: 1,
-                )),
-            Text(
-              "This is will be the page where you can control play/pause, volume control and all that stuff",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                height: 3,
-                fontSize: 25,
-                letterSpacing: 5,
-                wordSpacing: 10,
-              ),
+
+      //let's start by creating the main UI of the app
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xff3A527A),
+                Color(0xff3E75AC),
+              ]),
+          // responsible for the transclucentimage in the background
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            colorFilter: new ColorFilter.mode(
+                Colors.black.withOpacity(0.2), BlendMode.dstATop),
+            //change the image in here to change the background image
+            image: AssetImage(
+              "images/tablet_transparent.png",
             ),
-            Text(
-              "Swipe >>> to go back",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                height: 3,
-                fontSize: 30,
-                letterSpacing: 5,
-                wordSpacing: 10,
-              ),
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 48.0,
+          ),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 24.0,
+                ),
+                //Let's add the music cover
+                Center(
+                  child: Container(
+                    width: 280.0,
+                    height: 280.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        image: DecorationImage(
+                          image: AssetImage("images/img0.jpg"),
+                        )),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 18.0,
+                ),
+                Center(
+                  child: Text(
+                    "Manchester City 2 - 3 Manchester United",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 18.0,
+                ),
+                Center(
+                  child: Text(
+                    "Premier League 2017/18",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        //Let's start by adding the controller
+                        //let's add the time indicator text
+
+                        Container(
+                          width: 500.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "${position.inMinutes.toString()}:${pad(position.inSeconds.remainder(60).toString())}",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                              slider(),
+                              Text(
+                                "${musicLength.inMinutes.toString()}:${pad(musicLength.inSeconds.remainder(60).toString())}",
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              iconSize: 45.0,
+                              color: Colors.blue,
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.skip_previous,
+                              ),
+                            ),
+                            IconButton(
+                              iconSize: 62.0,
+                              color: Colors.blue[800],
+                              onPressed: () {
+                                //here we will add the functionality of the play button
+                                if (!playing) {
+                                  //now let's play the song
+                                  cache.play('demo.mp3');
+                                  setState(() {
+                                    playBtn = Icons.pause;
+                                    playing = true;
+                                  });
+                                } else {
+                                  _player.pause();
+                                  setState(() {
+                                    playBtn = Icons.play_arrow;
+                                    playing = false;
+                                  });
+                                }
+                              },
+                              icon: Icon(
+                                playBtn,
+                              ),
+                            ),
+                            IconButton(
+                              iconSize: 45.0,
+                              color: Colors.blue,
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.skip_next,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Center(),
-            ),
-          ],
+          ),
         ),
       ),
     );
